5fcb2d21122a8e1632261c19a3cb2e59e0eff758
jackvijayy <jackvijay1108@gmail.com>
2024-11-13 22:17:31 +0530

    langchain Integrate

--- app/api/pdf-loader/route.js
+++ app/api/pdf-loader/route.js
@@ -1,14 +1,34 @@
-import { NextResponse } from "next/server";
-import { WebPDFLoader } from "langchain/document_loaders/web/pdf";
 
+
+import { NextResponse } from "next/server";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 const pdfURl="https://brazen-sheep-58.convex.cloud/api/storage/2aba7b3a-5d83-4ff3-95c8-1122a689509a"
 export async function GET(req){
 
     const response =await fetch(pdfURl);
-    const data=await response.blob()
-    const loader=new WebPDFLoader(data);
+    const data=await response.blob();
+    const loader=new PDFLoader(data);
     const docs=await loader.load();
 
-    return NextResponse.json({result:"hello"})
+    let pdfTextContent="";
+    docs.forEach(docs=>{
+        pdfTextContent=pdfTextContent+docs.pageContent;
+    })
+
+    //text split
+
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 10,
+        chunkOverlap: 1,
+      });
+      const output = await splitter.createDocuments([pdfTextContent]);
+
+      let splitterList=[];
+      output.forEach(doc=>{
+        splitterList.push(docs.pageContent)
+      })
+
+    return NextResponse.json({result:output})
 }
